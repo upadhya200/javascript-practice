8907ded8c0d026488c48d2f546285c574482564a
amar <amarupadhya200@gmail.com>
2024-02-16 01:24:44 +0530

    Ex2 initiated

---
+++ ex2.js
@@ -0,0 +1,7 @@
+var btn_apply=document.querySelector("#apply");
+var btn_reset=document.querySelector("#reset");
+var color=document.getElementById("#colorSelect");
+
+btn_apply.addEventListener("click",function(){
+    btn_apply.style.backgroundColor=
+})
