7b1dbac4ba53684565ee5982c2618e140c2b0847
amar <amarupadhya200@gmail.com>
2024-02-17 16:18:48 +0530

    New functionality added

--- ex1.js
+++ ex1.js
@@ -1,4 +1,4 @@
-var btn_on=document.querySelector("#on");
+/*var btn_on=document.querySelector("#on");
 var btn_off=document.querySelector("#off");
 var blb =document.querySelector(".bulb");
 
@@ -8,4 +8,19 @@ btn_on.addEventListener("click",function(){
 
 btn_off.addEventListener("click",function(){
     blb.style.backgroundColor="white";
+})*/
+var blb =document.querySelector(".bulb");
+var swt = document.querySelector("#switch");
+var flag_value=0;
+
+swt.addEventListener("click",function(){
+    if (flag_value==0) {
+        blb.style.backgroundColor="Yellow"
+        console.log("yellow")
+        flag_value=1
+    } else {
+        blb.style.backgroundColor="transparent"
+        console.log("transparent")
+        flag_value=0
+    }
 })
--- ex2.js
+++ ex2.js
@@ -1,7 +1,15 @@
-var btn_apply=document.querySelector("#apply");
+/*var btn_apply=document.querySelector("#apply");
 var btn_reset=document.querySelector("#reset");
-var color=document.getElementById("#colorSelect");
+
 
 btn_apply.addEventListener("click",function(){
     btn_apply.style.backgroundColor=
-})
+})*/
+
+
+var color=document.getElementById("#colorSelect");
+console.log(color)
+
+//var c = color.innerHTML
+
+//console.log(c);
